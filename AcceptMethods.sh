ce90d2eb262baf0f5304324314425ece8c4f690a
Hudson Nowak <73683002+nowak4ux@users.noreply.github.com>
2020-12-15 23:14:35 -0300

    Add files via upload

---
+++ AcceptMethods.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if [[ $1 == "" || $2 == "" ]]
+then
+	echo -e "\n========================================================================\n"
+	echo "  ./AcceptMethods.sh [url] [wordlist] [user-agent] [file extension]"
+	echo -e "\n========================================================================\n"
+	exit
+fi
+
+echo -e "
+
+                                                                          ▒▒▒▒▒▒    
+                                                                          ▒▒▒▒▒▒▒▒░░
+                                                                          ▒▒▒▒  ▒▒▒▒
+                                                                          ▒▒▒▒▓▓▒▒▓▓
+                                                                        ▒▒▒▒▓▓██▓▓▓▓
+                                                                      ▒▒▒▒▓▓▓▓██▓▓▒▒
+                                                                    ░░▒▒▓▓██    ██  
+                                                                  ░░▒▒▓▓▓▓          
+                                                                ▒▒▒▒▓▓░░            
+                          ▒▒                                ▒▒▒▒▓▓▓▓                
+                      ▒▒▓▓▒▒                          ▓▓▒▒▒▒▓▓▓▓░░                  
+                          ▓▓▒▒                      ▓▓▓▓▓▓▓▓▓▓                      
+                            ▒▒                    ▓▓▓▓▓▓██▓▓                        
+                            ▓▓▒▒              ▒▒▓▓██████▓▓                          
+                              ▒▒            ▓▓▓▓▓▓████▒▒                            
+                              ▓▓▒▒      ░░▓▓▓▓▓▓████                                
+                                ▒▒▒▒  ▓▓▓▓████████                                  
+                              ░░▓▓▒▒▒▒▓▓▓▓████░░                                    
+                            ▓▓▒▒▓▓▒▒▒▒▓▓██▓▓                                        
+                          ▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓                                          
+                        ░░▒▒▒▒▒▒██████▓▓▒▒                                          
+                      ▒▒▒▒██▒▒▒▒▒▒▒▒██▓▓▒▒                                          
+                    ▒▒▒▒▒▒▓▓▓▓▒▒▓▓▓▓████▒▒▒▒                                        
+                  ▒▒▒▒▒▒▓▓▓▓██▓▓▓▓▓▓    ▓▓▒▒░░                                      
+                ▒▒▒▒▒▒██▓▓▒▒▓▓▓▓██        ▓▓▒▒░░                                    
+              ▓▓▒▒▓▓▓▓▒▒▒▒▓▓██░░            ▓▓▒▒                                    
+          ▒▒▓▓▒▒██▒▒▒▒▓▓▓▓██                  ██▒▒                                  
+        ▓▓▓▓▓▓▓▓▒▒▓▓▓▓████                      ▓▓▒▒▓▓░░                            
+      ▓▓▓▓▓▓▓▓▓▓██████▓▓                        ▓▓  ██                              
+  ▒▒▓▓▓▓▓▓▓▓████████                            ████                                
+"
+
+echo -e "\nHost: $1"
+
+if [[ $4 != "" ]]
+then
+	echo -e "File Extension(s): $4"
+fi
+
+if [[ $3 != "" ]]
+then
+	echo -e "User-Agent: $3\n"
+fi
+
+if [[ $3 != "" ]]
+then
+	if [[ $4 != "" ]]
+	then
+		gobuster dir -a $3 -w $2 -u $1 -x $4 -t64|grep "(Status"|cut -d "(" -f1 > gobuster_log.txt
+	fi
+	
+	gobuster dir -a $3 -w $2 -u $1 -t64|grep "(Status"|cut -d "(" -f1 > gobuster_log.txt
+fi
+
+gobuster dir -w $2 -u $1 -t64|grep "(Status"|cut -d "(" -f1 > gobuster_log.txt
+
+for lp in $(cat gobuster_log.txt)
+do
+	if [[ $3 != "" ]]
+	then
+		curl -X OPTIONS $1$lp -v -A $3 1>/dev/null 2>>curl_log.txt;
+	fi
+
+	curl -X OPTIONS $1$lp -v 1>/dev/null 2>>curl_log.txt;
+done
+
+echo -e "\n================================="
+cat curl_log.txt|grep 'OPTIONS\|Host:\|Allow:'|sed s/../"==========================\n"/
+echo -e "=================================\n"
+
+rm -rf gobuster_log.txt curl_log.txt
